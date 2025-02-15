415f6fbfd9e04dbe54eca9f37ecea77f0c7298e5
Gerald Okafor <zobaimpact@gmail.com>
2025-02-15 14:50:54 +0100

    added routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import News from "./components/News/index";
+import NewYorkTimes from "./components/News/NewYork/index";
+import NewsApi from "./components/News/NewsApi/index";
 import NavBar from "./components/Navbar/index";
 
 function App() {
+  console.log("App Component Loaded");
   return (
     <>
       <NavBar />
       <div className="wrapper">
-        <News />
+        <Routes>
+          <Route path="/" element={<NewYorkTimes />} />
+          <Route path="/new-api" element={<NewsApi />} />
+          <Route path="/bbc-news" element={<NewYorkTimes />} />
+          <Route path="*" element={<h2 className="text-center text-red-500">404 - Page Not Found</h2>} />
+        </Routes>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store} from "./redux/store/configureStore.ts";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
