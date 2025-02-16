cdce2f9805da9c4fc41255bec289f136869f980b
Gerald Okafor <zobaimpact@gmail.com>
2025-02-16 22:50:41 +0100

    added redux-persist and react-toastify setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NewYorkTimes from "./components/News/NewYork/index";
 import GuardianNews from "./components/News/Guardian/index";
+import NewsSearch from "./components/News/NewsSearch/index";
 import NewsApi from "./components/News/NewsApi/index";
 import NavBar from "./components/Navbar/index";
 
+
 function App() {
-  console.log("App Component Loaded");
   return (
     <>
       <NavBar />
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<NewYorkTimes />} />
           <Route path="/new-api" element={<NewsApi />} />
           <Route path="/guardian-news" element={<GuardianNews />} />
+          <Route path="/search-result" element={<NewsSearch />} />
           <Route path="*" element={<h2 className="text-center text-red-500">404 - Page Not Found</h2>} />
         </Routes>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,30 @@
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
-import { store} from "./redux/store/configureStore.ts";
+import { store, persistor } from "./redux/store/configureStore";
 import { BrowserRouter } from "react-router-dom";
-import App from './App.tsx'
-
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ToastContainer
+        position="top-right"
+        autoClose={500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
