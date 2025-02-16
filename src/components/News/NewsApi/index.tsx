8b6c31a74baa1f7e8483089190b673aca1fe27cc
Gerald Okafor <zobaimpact@gmail.com>
2025-02-16 22:53:05 +0100

    added search result component, code clean up, conditional component renedering

--- src/components/News/NewsApi/index.tsx
+++ src/components/News/NewsApi/index.tsx
@@ -2,27 +2,31 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../redux/store/configureStore";
 import { fetchNewsApi } from "../../../redux/slice/newsSlice";
-import Card from "../../Card/index";
+import Card from "../../Card";
 import { Loader } from "../../Loader";
+import NewsFilter from "../../Filters";
 
-const NewsApi = () => {
+const NewsApi: React.FC = () => {
+  // Use the typed dispatch for Redux actions
   const dispatch = useDispatch<AppDispatch>();
 
-  const { articles,  status, error } = useSelector(
+  // Retrieve news articles, status, and error from the Redux store.
+  const { articles, status, error } = useSelector(
     (state: RootState) => state.news
   );
 
+  // On component mount, fetch initial articles from the News API.
   useEffect(() => {
     dispatch(fetchNewsApi());
   }, [dispatch]);
 
   return (
-    <div
-      className="columns-2xs"
-    >
-      {status === "loading" && <Loader/>}
+    <div className="columns-2xs">
+      {articles && <NewsFilter source="newsapi" />}
+      {/* Display loader, error, or the news cards based on the current state */}
+      {status === "loading" && <Loader />}
       {status === "failed" && <p>Error: {error}</p>}
-      {status === "succeeded" && articles  && <Card news={articles} />}
+      {status === "succeeded" && articles && <Card news={articles} />}
     </div>
   );
 };
