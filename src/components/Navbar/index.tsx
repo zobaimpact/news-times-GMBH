ed5029749806dd7769405e937162ec5325e27488
Gerald Okafor <zobaimpact@gmail.com>
2025-02-16 22:51:31 +0100

    code clean up

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,6 @@ import {
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
-  Input,
 } from "@heroui/react";
 
 // Define the menu item type
@@ -101,21 +100,6 @@ const NavBar: React.FC = () => {
             </Link>
           </NavbarItem>
         ))}
-      </NavbarContent>
-
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[20rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper:
-              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          type="search"
-        />
       </NavbarContent>
 
       <NavbarMenu>
